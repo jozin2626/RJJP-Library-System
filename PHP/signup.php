f02fa49dd7b5e129f039b96e568b6775bc613081
Justin Vance <64617790+jozin2626@users.noreply.github.com>
2022-05-01 15:25:41 -0400

    Update signup.php

--- PHP/signup.php
+++ PHP/signup.php
@@ -3,13 +3,25 @@
 <head>
     <meta charset="utf-8" />
     <link rel="stylesheet" href="styles.css" />
+    <link rel="stylesheet" 
+        href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" 
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" 
+        crossorigin="anonymous">
     <title> Library Management System</title>
     
 </head>
 
 <body>
+<nav id = "navbar">
+    <li> <button type = "button " onclick="home()" id = "logout"> Logout </button></li>
+    <script>
+      function home() {
+        window.location.href='index.php';
+      }
+    </script>
+    <li> <button type = "button " id = "accountView">View Account</button> </li>
 
-
+    </nav>
 <form action="includes/signup.inc.php" method="POST">
     <div class="UpperScreen">
         <h1>
@@ -17,43 +29,35 @@
 
         </h1>
         <h2>
-            Login Access
+            Account Creation
         </h2>
     </div>
 
+    <div class="container-fluid text-center">
     <br>
-    <div>
-        <label for="userid">User ID</label>
-        <input type="text" name="USERID" placeholder="userid">
-    </div>
+        <label for="FIRSTNAME">First Name</label><br>
+        <input type="text" id="fname" name="FIRSTNAME" placeholder="First name">
     <br>
-    <div>
-        <label for="fname">First Name</label>
-        <input type="text" name="FIRSTNAME" placeholder="firstname">
-    </div>
+        <label for="LASTNAME">Last Name</label><br>
+        <input type="text" id="lname" name="LASTNAME" placeholder="Last name">
     <br>
-    <div>
-        <label for="lname">Last Name</label>
-        <input type="text" name="LASTNAME" placeholder="lastname">
-    </div>
+        <label for="USERNAME">Username</label><br>
+        <input type="text" id="uName" name="USERNAME" placeholder="Username">
     <br>
-    <div>
-        <label for="fname">Username</label>
-        <input type="text" name="USERNAME" placeholder="username">
-    </div>
+        <label for="PASSWORD">Password</label><br>
+        <span>Passwords must be at least 8 characters, and contain at least one letter, and number.</span><br>
+        <input type="password" id="password" name="PASSWORD" placeholder="Password">
     <br>
-    <div>
-        <label for="pass">Password</label>
-        <input type="text" name="PASSWORD" placeholder="password">
-    </div>    
+        <label for="PASSWORDCONFIRM">Confirm Password</label><br>
+        <input type="password" id="passConfiirm" name="PASSWORDCONFIRM" placeholder="Password">
     <br>
-    <div>
-        <label for="email">Email</label>
-        <input type="email" name="EMAIL" placeholder="email">
-    </div>
+        <label for="EMAIL">Email</label><br>
+        <input type="email" name="EMAIL" placeholder="Email">
     <br>
-    <div>
-        <button type="submit" onclick="validate()">Validate</button>
+        <label for="ADDRESS">Password</label><br>
+        <input type="text" name="ADDRESS" placeholder="Address">
+    <br><br>
+        <button type="submit" class="btn btn-secondary" onclick="validate()">Create Account</button>
     </div>
 </form>
 
